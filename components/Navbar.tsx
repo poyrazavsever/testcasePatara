3f494148685a30758cda08d909673720f2c3e543
poyrazavsever <poyrazavsever@hotmail.com>
2025-05-10 19:29:31 +0300

    style: updated padding n color in navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -25,20 +25,20 @@ const Navbar = () => {
           placeholder="Enter Accounts, Platforms, NFTs, Token"
           className="pl-10 pr-20 bg-[#1e1e1e] text-sm text-white border-none focus-visible:ring-0"
         />
-        <div className="absolute right-3 top-1/2 -translate-y-1/2 text-xs text-gray-400 bg-gray-800 px-2 py-0.5 rounded">
+        <div className="absolute right-3 top-1/2 -translate-y-1/2 text-xs text-gray-400 bg-neutral-900 px-2 py-0.5 rounded">
           ⌘ K
         </div>
       </div>
 
       {/* Sağ taraf */}
-      <div className="flex items-center gap-4">
+      <div className="flex items-center gap-2">
         <div className='cursor-pointer p-2 rounded-sm bg-neutral-800 hover:bg-neutral-700 transition-all duration-200'>
           <Bell className="w-5 h-5" />
         </div>
         <div className='cursor-pointer p-2 rounded-sm bg-neutral-800 hover:bg-neutral-700 transition-all duration-200'>
           <Settings className="w-5 h-5" />
         </div>
-        <Button className="cursor-pointer bg-blue-600 text-white hover:bg-blue-500 transition-all duration-200 text-sm px-4 py-2">
+        <Button className="bg-blue-600 text-white hover:bg-blue-500 transition-all duration-200 text-sm px-4 py-2">
           Connect/Sign in
         </Button>
       </div>
